eba88ceb65f944a4dae81f49bc64237fb8e42350
RyanTytka <rdt7867@rit.edu>
2026-04-03 10:54:32 -0500

    bug fixes

--- Assets/Scripts/buttonTextMove.cs
+++ Assets/Scripts/buttonTextMove.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class buttonTextMove : MonoBehaviour
 {
     public void MoveText(float amount)
     {
+        //make sure button is enabled
+        if (GetComponent<Button>().interactable == false) return;
+        //move text
         gameObject.transform.GetChild(0).GetComponent<RectTransform>().localPosition = new Vector3(0, amount, 0);
         // deselect this button after clicked
         if(amount == 0)
