4ee33e802b0b6bf0c939b20e604d3f2af47df000
Tytka <e405920@us.lmco.com>
2026-04-16 17:30:21 -0500

    notes

--- Assets/Scripts/tutorialManager.cs
+++ Assets/Scripts/tutorialManager.cs
@@ -7,19 +7,24 @@ using DG.Tweening;
 
 public class tutorialManager : MonoBehaviour
 {
-    //1 Select a club(use driver since it goes away)
-    //2 This is path ball will take.Watch out for hazards
-    //3 Play an ability/select a ball
-    //4 Here is the flag, try to make it onto the green in as few shots as you can
-    //5 This is putting meter. Putt is auto once you land on the green
-    //6 This is your rival, each course has one. score and ability.
-    //7 after done with hole: add new card or tees.Show rarities.Shop after each course
-
-    //1 - "Play through 5 courses of 9 holes each. If you dont score lower than your rival on each course, you lose."
-    //2 - "You start each hole with 5 cards in hand, plus your drivers. Each time you swing, you draw a card."
-    //3 - "There are 4 types of card. Clubs, Balls, Abilities, Caddies.
+    //"Play through 5 courses of 9 holes each. If you dont score lower than your rival on each course, you lose."
+    //"You start each hole with 5 cards in hand, plus your drivers. Each time you swing, you draw a card. Click the Mulligan button to draw a card, but it will cost you a stroke. Get the ball into the hole in as few swings as you can."
+    //    "After each hole you can collect tees, based on how well you did, or add a new card to your deck. After each course, you can spend tees at the Pro Shop."
+    //"There are 4 types of card. Clubs, Balls, Abilities, Caddies.
     //      Clubs - You need a club to hit the ball. Click a club to select it for your next swing.
-    //      Balls - Click a ball
+                //There are 3 types of clubs: Woods, Irons, and Wedges. Woods are used for distance, and wedges are used for close range accuracy.
+                //Some Woods are Drivers, which means they start in your hand but can only be used on your first shot.
+    //      Balls - Click a ball to select it and give this swing special effects. No more than 1 ball can be used each swing.
+    //      Abilities - Drag them to the middle of the screen to play them. They give you a one time bonus then are discarded. You can play as many abilities per turn as you want.
+    //      Caddies - Drag them to the middle of the screen to play them. They stay in play for the rest of the hole, providing beneficial effects while in play.
+    //"Each card has a rarity: Rookie, Pro, Legend. Spend tees at the Pro Shop to upgrade a card's rarity for more powerful effects.
+    //Watch out for hazards on the course:
+        //Rolling over rough reduces the power of your next shot.
+        //Sand stops your ball and discards a random card from your hand.
+        //Water stops your ball and costs you an extra stroke, then puts your ball on the next dry spot.
+        //Once you make it to the green, you will automatically putt it into the hole to finish the hole. 
+            // The further you are form the hole, the more putts it will take. Hold tab to show ???. Look at Meter.
+    //This is your rival, each course has one. score and ability.
 
 
     public GameObject[] panels; //Each panel is a page of the tutorial
